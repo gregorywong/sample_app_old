e8ac54ae9f2fd5dfc351cd4f1f8aaf8078db813b
Greg Wong <gwong1990@gmail.com>
2012-07-25 21:52:06 -0700

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,14 +1,3 @@
-# == Schema Information
-#
-# Table name: users
-#
-#  id         :integer         not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime        not null
-#  updated_at :datetime        not null
-#
-
 require 'spec_helper'
 
 describe User do
@@ -22,6 +11,7 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
   it { should be_valid }
 
@@ -115,6 +105,10 @@ describe User do
     end
   end
 
+  describe "remember token" do
 
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
 
 end
